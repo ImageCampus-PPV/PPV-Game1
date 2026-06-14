6e028f910b8114f34d7c5427b7f847a8d6f597d9
sofi <genuinedistinctclearcapital@gmail.com>
2026-06-14 20:24:17 -0300

    Make the embers attack line have a width

--- Assets/Scripts/Characters/CharacterAbilities/Dragon/EmbersAttackStrategy.cs
+++ Assets/Scripts/Characters/CharacterAbilities/Dragon/EmbersAttackStrategy.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/Attacks/Air Embers (Y)")]
 public class EmbersAttackStrategy : AttackStrategy
 {
-    [SerializeField] private float _range = 5f;
-    [SerializeField] private float _groundRadius = 2f;
+    [SerializeField] private float _beamRange = 5f;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _burnDamagePerSecond = 3f;
     [SerializeField] private float _burnDuration = 3f;
     [SerializeField] private float _fireSpreadLength = 5f;
     [SerializeField] private float _fireSpreadHeight = 1f;
+    [SerializeField] private float _beamLineThickness = 0.05f;
+    [SerializeField] private float _beamWidth = 0.5f;
     private Vector2 _currentAim;
 
     private RuntimeDebugVisual _debugVisual;
@@ -42,19 +43,28 @@ public class EmbersAttackStrategy : AttackStrategy
 
         _currentAim = character.CurrentAimDir;
 
-        _debugVisual.DrawRay(character.transform.position, _currentAim, _range, Color.red, Time.deltaTime);
+        Vector2 start = character.transform.position;
 
-        RaycastHit2D[] hits = Physics2D.RaycastAll(character.transform.position, _currentAim, _range, enemyLayer | _groundLayer);
+        RaycastHit2D groundHit = Physics2D.Raycast(start, _currentAim, _beamRange, _groundLayer);
+
+        Vector2 beamCenter = start + _currentAim * (_beamRange * 0.5f);
+        Vector2 beamSize = new(_beamRange, _beamWidth);
+        float beamAngle = Mathf.Atan2(_currentAim.y, _currentAim.x) * Mathf.Rad2Deg;
+
+        if (_debugVisual)
+            _debugVisual.DrawOrientedBox(beamCenter, beamSize, beamAngle, Color.red, Time.deltaTime, _beamLineThickness <= 0f ? 0.05f : _beamLineThickness);
+
+        Collider2D[] hits = Physics2D.OverlapBoxAll(beamCenter, beamSize, beamAngle, enemyLayer | _groundLayer);
 
         foreach (var hit in hits)
         {
-            if (((1 << hit.collider.gameObject.layer) & enemyLayer) != 0)
+            if (((1 << hit.gameObject.layer) & enemyLayer) != 0)
             {
-                IDamageable damageable = hit.collider.GetComponent<IDamageable>();
+                IDamageable damageable = hit.GetComponent<IDamageable>();
                 //mientras el ascuas toque al enemigo, le hace daño
                 damageable?.TakeDamage(damage * Time.deltaTime);
 
-                if (hit.collider.TryGetComponent<IStatusEffectReceiver>(out var receiver))
+                if (hit.TryGetComponent<IStatusEffectReceiver>(out var receiver))
                 //y, además, le aplica el efecto de quemadura por x tiempo.
                 {
                     if (!receiver.HasEffect<BurnEffect>())
@@ -62,16 +72,14 @@ public class EmbersAttackStrategy : AttackStrategy
                 }
             }
 
-            if (((1 << hit.collider.gameObject.layer) & _groundLayer) != 0)
+            if (groundHit.collider != null)
             {
-                _debugVisual.DrawCircle(hit.point, _groundRadius, Color.coral, Time.deltaTime);
-
                 Vector2 rangeSize = new(_fireSpreadLength, _fireSpreadHeight);
 
-                Collider2D[] groundAoeHits = Physics2D.OverlapBoxAll(hit.point, rangeSize, 0f, enemyLayer);
+                Collider2D[] groundAoeHits = Physics2D.OverlapBoxAll(groundHit.point, rangeSize, 0f, enemyLayer);
                 DealDamageToTargets(groundAoeHits, damage * Time.deltaTime);
 
-                _debugVisual.DrawBox(hit.point, rangeSize, Color.orange, Time.deltaTime);
+                _debugVisual.DrawBox(groundHit.point, rangeSize, Color.orange, Time.deltaTime);
 
                 break;
             }
