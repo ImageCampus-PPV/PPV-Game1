f2d803cd9f405e89b18bebe625538037d5c06a87
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 14:59:23 -0300

    Update CoopCameraController.cs

--- Assets/Scripts/CoopCamera/CoopCameraController.cs
+++ Assets/Scripts/CoopCamera/CoopCameraController.cs
@@ -1,61 +1,43 @@
-using System;
 using UnityEngine;
-using ImageCampus.ToolBox.Services;
-using NUnit.Framework;
+using Assets.Scripts.Entities;
 using System.Collections.Generic;
+using ImageCampus.ToolBox.Services;
 
-[Serializable]
-public class CoopCameraSettings
+public class CoopCameraController : IInitiable, ITickable, IService
 {
-    public Vector3 offset;
-    public float speed;
-}
+    public bool IsPersistance => false;
 
-public interface ICoopCameraService : IService
-{
-    CameraBounds GetBounds();
-}
+    EntityRegistry EntityRegistry => ServiceProvider.Instance.GetService<EntityRegistry>();
 
-public class CoopCameraController : MonoBehaviour, ICoopCameraService
-{
+    private float speed = 10.0f;
+    private Vector3 offset = Vector3.forward * -10.0f;
+
     [SerializeField] private Camera _cam;
-    [SerializeField] private PlayersContainer _container;
-    [SerializeField] private CoopCameraSettings _settings = new();
+
     [SerializeField] private float _boundsMargin = 0.5f;
 
     private CoopCameraModel _model;
     private readonly List<Vector3> _positions = new();
 
-    public bool IsPersistance => false;
+    public Camera Camera => _cam;
 
-    private void Awake()
+    public void Init()
     {
-        _model = new CoopCameraModel(_settings, _boundsMargin);
+        _model = new CoopCameraModel(_boundsMargin);
 
-        ServiceProvider.Instance.AddService<ICoopCameraService>(this);
-
-        if (_container == null)
-            Debug.Log($"No {nameof(PlayersContainer)} inserted in {nameof(CoopCameraController)}");
-
-        if (_cam == null)
-            _cam = GetComponent<Camera>();
-
-        SnapToPlayer();
+        _cam = Camera.main;
     }
 
-    private void OnDestroy()
+    public void LateInit()
     {
-        ServiceProvider.Instance.RemoveService<ICoopCameraService>();
+
     }
 
-    private void LateUpdate()
+    public void Tick(float deltaTime)
     {
-        if (_container == null || _container.Players.Count == 0)
-            return;
-
         _positions.Clear();
 
-        foreach (Character player in _container.Players)
+        foreach (Character player in EntityRegistry.FilterEntities<Character>())
             _positions.Add(player.transform.position);
 
         Vector3 targetCentroid = _model.FindCentroid(_positions);
@@ -80,20 +62,25 @@ public class CoopCameraController : MonoBehaviour, ICoopCameraService
         };
     }
 
-    private void SnapToPlayer()
-    {
-        if (_container.Players.Count == 0)
-            return;
-
-        Vector3 startPos = _container.Players[0].transform.position;
-
-        transform.position = startPos += _settings.offset;
-    }
-
     private void GoToPos(Vector3 pos)
     {
-        Vector3 desiredPos = pos + _settings.offset;
-        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, _settings.speed * Time.deltaTime);
-        transform.position = smoothedPos;
+        Vector3 desiredPos = pos + offset;
+        Vector3 smoothedPos = Vector3.Lerp(_cam.transform.position, desiredPos, speed * Time.deltaTime);
+        _cam.transform.position = smoothedPos;
+    }
+
+    public void SetOffset(Vector3 offset)
+    {
+        this.offset = offset;
+    }
+
+    public void SetOffset(float offsetZ)
+    {
+        offset = Vector3.forward * -Mathf.Abs(offsetZ);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
     }
 }
