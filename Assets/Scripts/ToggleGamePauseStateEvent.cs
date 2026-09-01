8a02812e67263b70e90f3524dc7baeedf2c4d3c5
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 16:06:01 -0300

    Create ToggleGamePauseStateEvent.cs

---
+++ Assets/Scripts/ToggleGamePauseStateEvent.cs
@@ -0,0 +1,14 @@
+﻿using ImageCampus.ToolBox.Events;
+
+public struct ToggleGamePauseStateEvent : IEvent
+{
+    public void Assign(params object[] parameters)
+    {
+
+    }
+
+    public void Reset()
+    {
+
+    }
+}
