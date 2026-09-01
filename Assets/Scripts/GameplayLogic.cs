da5f080cb73e98e97adbf951653947ff91d3fb87
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 16:13:50 -0300

    Create GameplayLogic.cs

---
+++ Assets/Scripts/GameplayLogic.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.Entities;
+using ImageCampus.ToolBox.Services;
+using System;
+using Systems;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayLogic : IInitiable, ITickable, IDisposable
+{
+    private EntityRegistry EntityRegistry => ServiceProvider.Instance.GetService<EntityRegistry>();
+    private EntityFactory EntityFactory => ServiceProvider.Instance.GetService<EntityFactory>();
+    private CoopCameraController CoopCameraController => ServiceProvider.Instance.GetService<CoopCameraController>();
+    private ControllerMapping ControllerMapping => ServiceProvider.Instance.GetService<ControllerMapping>();
+
+    private SceneRef _gamePlayScene;
+
+    public GameplayLogic(SceneRef _gamePlayScene)
+    {
+        this._gamePlayScene = _gamePlayScene;
+    }
+
+    public void Init()
+    {
+        SceneManager.LoadScene(_gamePlayScene.Index, LoadSceneMode.Additive);
+
+        ServiceProvider.Instance.AddService<ControllerMapping>(new ControllerMapping());
+        ServiceProvider.Instance.AddService<EntityFactory>(new EntityFactory());
+        ServiceProvider.Instance.AddService<EntityRegistry>(new EntityRegistry());
+        ServiceProvider.Instance.AddService<CoopCameraController>(new CoopCameraController());
+        ServiceProvider.Instance.AddService<CoopCameraController>(new Inventory());
+
+        ControllerMapping.Init();
+        EntityFactory.Init();
+        CoopCameraController.Init();
+    }
+
+    public void LateInit()
+    {
+        ControllerMapping.LateInit();
+        EntityFactory.LateInit();
+        CoopCameraController.LateInit();
+
+        EntityFactory.Create<Mecha>();
+        EntityFactory.Create<Dragon>();
+    }
+
+    public void Tick(float deltaTime)
+    {
+        ControllerMapping.Tick(deltaTime);
+        CoopCameraController.Tick(deltaTime);
+    }
+
+    public void Dispose()
+    {
+        SceneManager.UnloadSceneAsync(_gamePlayScene.Index);
+        ServiceProvider.Instance.ClearAllServices();
+    }
+}
