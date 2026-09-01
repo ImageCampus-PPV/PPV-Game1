178e18bbf76fccd3c0ba57c85ccad42b17e3a9cd
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 16:13:26 -0300

    Create PrefabsRegistry.cs

---
+++ Assets/Scripts/PrefabsRegistry.cs
@@ -0,0 +1,18 @@
+﻿using ImageCampus.ToolBox.Services;
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct PrefabByName
+{
+    public string TypeName; 
+    public GameObject gameObject;
+}
+
+[CreateAssetMenu(fileName = nameof(PrefabsRegistry), menuName =  nameof(ScriptableObject) +  "/" + nameof(PrefabsRegistry), order = 1)]
+public class PrefabsRegistry : ScriptableObject, IService
+{
+    public bool IsPersistance => true;
+
+    public PrefabByName[] prefab;
+}
