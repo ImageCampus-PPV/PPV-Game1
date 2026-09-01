5e23b9fb2e140d2f174d941ffbf9b046fdedbe0b
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 17:06:24 -0300

    Update OffCameraCondition.cs

--- Assets/Scripts/Characters/Enemies/OffCameraCondition.cs
+++ Assets/Scripts/Characters/Enemies/OffCameraCondition.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy/Condition/OffCamera")]
 public class OffCameraCondition : Condition<IEnemyContext>
 {
-    private CoopCameraController CameraController => ServiceProvider.Instance.ContainsService<CoopCameraController>() ? ServiceProvider.Instance.GetService<CoopCameraController>() : null;
+    private CoopCameraController CameraController => ServiceProvider.Instance.GetService<CoopCameraController>();
 
     public override bool Evaluate(IEnemyContext context)
     {
-        if (!CameraController)
-        {
-            Debug.LogError("No camera controller found in OffCamera condition for enemy " + context.Transform.name);
-            return false;
-        }
-
         return !CameraController.IsInCameraBounds(context.Transform.position);
     }
 }
