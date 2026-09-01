e7eae116ca8bb892870f8e1caac50daeb694fe06
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 16:14:35 -0300

    Create InputDeviceDisconnectRequestEvent.cs

---
+++ Assets/Scripts/InputDeviceDisconnectRequestEvent.cs
@@ -0,0 +1,18 @@
+﻿using ImageCampus.ToolBox.Events;
+
+namespace Pawgineers.Gameplay.Inputs
+{
+    public struct InputDeviceDisconnectRequestEvent : IEvent
+    {
+        public uint entityID;
+        public void Assign(params object[] parameters)
+        {
+            entityID = (uint)parameters[0];
+        }
+
+        public void Reset()
+        {
+            entityID = default(uint);
+        }
+    }
+}
