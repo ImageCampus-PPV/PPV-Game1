a0b1546934b1f9c6f43f8285804b0c4c5d3f6894
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-01 16:13:58 -0300

    Create DeviceMappingRequestEvent.cs

---
+++ Assets/Scripts/DeviceMappingRequestEvent.cs
@@ -0,0 +1,17 @@
+﻿using ImageCampus.ToolBox.Events;
+using ImageCampus.ToolBox.Services;
+
+public struct DeviceMappingRequestEvent : IEvent
+{
+    public uint entityID;
+
+    public void Assign(params object[] parameters)
+    {
+        entityID = (uint)parameters[0];
+    }
+
+    public void Reset()
+    {
+        entityID = default(uint);
+    }
+}
